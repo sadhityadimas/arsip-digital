8f87e193eed8149bc3cd0b148f7a629c7c3cc381
sadhityadimas <s.adhitya.dimas@gmail.com>
2022-07-12 10:46:01 +0700

    first commit

--- media_arsip.py
+++ media_arsip.py
@@ -59,11 +59,15 @@ with st.sidebar:
     st.write("\n")
     st.write("\n")
     st.write("\n")
-    new_title = '<p style="font-family:sans-serif; font-size: 8px;"> Program Aktualisasi Diklatsar CPNS BPK 2022 </p>'
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    new_title = '<p style="font-family:sans-serif; font-size: 10px;"> Program Aktualisasi Diklatsar CPNS BPK 2022 </p>'
     st.markdown(new_title, unsafe_allow_html=True)
-    new_title2 = '<p style="font-family:sans-serif; font-size: 8px;"> Mentor Joko Sancoyo </p>'
+    new_title2 = '<p style="font-family:sans-serif; font-size: 10px;"> Mentor Joko Sancoyo </p>'
     st.markdown(new_title2, unsafe_allow_html=True)
-    new_title3 = '<p style="font-family:sans-serif; font-size: 8px;"> Mentee Dimas Adhitya Suwarto </p>'
+    new_title3 = '<p style="font-family:sans-serif; font-size: 10px;"> Mentee Dimas Adhitya Suwarto </p>'
     st.markdown(new_title3, unsafe_allow_html=True)
 
 
